5befd83f497172b554d09d95bde8584d7bde2616
Unknown <timothy.hyndman@gmail.com>
2017-10-26 12:24:01 +1100

    Add size option to fa

--- R/fa.R
+++ R/fa.R
@@ -3,11 +3,22 @@
 #' Inserts the icon with given name into R Markdown
 #' 
 #' @param name The name of the icon
+#' @param size Size of the icon relative to font size. Options are lg (33% 
+#' increase), 2x, 3x, 4x, or 5x.
 #' 
 #' @export
 
-fa <- function(name) {
-  string <- paste("<i class='fa fa-", name, "'></i>", sep = "")
+fa <- function(name, size = "default") {
+
+	size_append <- switch(size,
+												default = "",
+												lg = " fa-lg",
+												`2x` = " fa-2x",
+												`3x` = " fa-3x",
+												`4x` = " fa-4x",
+												`5x` = " fa-5x")
+
+  string <- paste("<i class='fa fa-", name, size_append, "'></i>", sep = "")
   
   structure(string, class = "icon")
 }
