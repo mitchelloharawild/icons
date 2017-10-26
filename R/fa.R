7789efd47b8d098813d0afa025e7542ac1b79fe7
Unknown <mitch.ohara-wild@monash.edu>
2017-10-26 13:33:47 +1100

    Added structure for pdf/html detection

--- R/fa.R
+++ R/fa.R
@@ -1,11 +1,11 @@
 #' Insert Icon From Font Awesome
-#' 
+#'
 #' Inserts the icon with given name into R Markdown
-#' 
+#'
 #' @param name The name of the icon
-#' @param size Size of the icon relative to font size. Options are lg (33% 
+#' @param size Size of the icon relative to font size. Options are lg (33%
 #' increase), 2x, 3x, 4x, or 5x.
-#' 
+#'
 #' @export
 
 fa <- function(name, size = "default") {
@@ -18,9 +18,15 @@ fa <- function(name, size = "default") {
 												`4x` = " fa-4x",
 												`5x` = " fa-5x")
 
-  icon <- htmltools::tags$i(class = paste('fa fa-', name, size_append, sep = ""))
-
-	header <- htmltools::singleton(htmltools::tags$head(rmarkdown::html_dependency_font_awesome()))
-
-  htmltools::tagList(header, icon)
+	if(knitOutputType() == "pdf_document"){
+	  stop("PDF not supported")
+	}
+	else if(knitOutputType() == "word_document"){
+	  warning("Word output not supported")
+	}
+	else{
+	  icon <- htmltools::tags$i(class = paste('fa fa-', name, size_append, sep = ""))
+	  header <- htmltools::singleton(htmltools::tags$head(rmarkdown::html_dependency_font_awesome()))
+	  htmltools::tagList(header, icon)
+	}
 }
