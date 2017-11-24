26a7bd67e94e156c2f541731a6564dd068bc9c11
earowang <earo.wang@gmail.com>
2017-11-24 14:28:12 +1100

    return unknown_document

--- R/print.R
+++ R/print.R
@@ -2,8 +2,10 @@ knitOutputType <- function() {
   output <- rmarkdown::yaml_front_matter(knitr::current_input())$output
   if (is.list(output)) {
       return(names(output)[1])
+  } else if (length(output) == 0) {
+    return("unknown_document")
   } else {
-      return(output[1])
+    return(output[1])
   }
 }
 
