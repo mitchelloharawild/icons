257b7984612b665935390d3655ee05e8a53665d6
Unknown <mitch.ohara-wild@monash.edu>
2018-05-10 17:51:27 +1000

    Fixed colour styler

--- R/style.R
+++ R/style.R
@@ -1,7 +1,7 @@
 font_style <- function(x){
   out <- NULL
   if(!is.null(x$options$colour)){
-    out <- paste0(out, ";color:", x$options$colour)
+    out <- paste0(out, "color:", x$options$colour,";")
   }
   out
 }
