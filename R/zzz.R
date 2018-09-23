ac7b38fc003757f9bac261e276a540062e7bbaf6
Mitchell <mitch.ohara-wild@monash.edu>
2018-09-23 16:32:10 +1000

    Improved option :mag:

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.icon <- list(
+    icon.path = rappdirs::user_data_dir("icon")
+  )
+  toset <- !(names(op.icon) %in% names(op))
+  if (any(toset)) options(op.icon[toset])
+
+  invisible()
+}
