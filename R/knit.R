bdf4bcd2fc6f03e972be046370c78613a239b489
Mitchell <mitch.ohara-wild@monash.edu>
2019-07-16 05:31:30 +1000

    Improved detection of output format :star2:

--- R/knit.R
+++ R/knit.R
@@ -1,28 +1,18 @@
-knitOutputType <- function() {
-  output <- tryCatch(
-    rmarkdown::yaml_front_matter(knitr::current_input())$output,
-    error = function(e) "console"
-  )
-  if (is.list(output)) {
-    return(names(output)[1])
-  } else if (length(output) == 0) {
-    return("unknown_document")
-  } else {
-    return(output[1])
-  }
-}
-
 #' @importFrom knitr knit_print
 #' @export
 knit_print.icon <- function(x, ...) {
-  if (knitOutputType() %in% c("pdf_document", "beamer", "pdf_document2")) {
-    if (length(x$name) > 1) {
-      stop("Icons for PDF formats are currently not supported", call = FALSE)
-    }
-    # knitr::asis_output(paste0("\\includesvg{", x$name, "}"), meta = list(rmarkdown::latex_dependency("svg")))
-  } else if (knitOutputType() == "word_document") {
-    stop("Icons for word document formats are currently not supported", call = FALSE)
-  } else {
+  out_type <- knitr::opts_knit$get("rmarkdown.pandoc.to")
+  if(is.null(out_type)){
+    warn("Could not detect output format, please use `rmarkdown::render()` to knit the document.")
+    return(knitr::asis_output(""))
+  }
+  if(out_type == "html"){
     knitr::asis_output(htmltools::htmlPreserve(gsub('"', "'", format(x))))
+  }
+  else if(out_type == "pdf"){
+    stop("Icons for PDF formats are currently not supported", call = FALSE)
+  }
+  else {
+    stop("Icons for this format is currently not supported", call = FALSE)
   }
 }
