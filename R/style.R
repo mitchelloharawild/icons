f98a3a1106f8b7863e4b48e99ab67c80dcf7b18c
Mitchell <mitch.ohara-wild@monash.edu>
2019-10-01 21:13:30 +1000

    Updated docs :black_nib:

--- R/style.R
+++ R/style.R
@@ -1,13 +1,18 @@
+#' Customise the style of an icon
+#'
+#' @param scale Scaled size of the icon.
+#' @param fill The colour for the fill.
+#'
 #' @export
-style <- function(x, height = NULL, fill = NULL){
+style <- function(x, scale = NULL, fill = NULL){
   style <- x$attribs$style
   if(!is.null(fill)){
     style <- gsub("fill:[^;]*;", "", style)
     style <- paste0(style, "fill:", fill, ";")
   }
-  if(!is.null(height)){
+  if(!is.null(scale)){
     style <- gsub("height:[^;]*;", "", style)
-    style <- paste0(style, "height:", height, "em;")
+    style <- paste0(style, "height:", scale, "em;")
   }
   x$attribs$style <- style
   x
