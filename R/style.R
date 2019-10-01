2385ad3384d0c4131e14025079d830c766a03dc4
Mitchell <mitch.ohara-wild@monash.edu>
2019-10-01 21:46:07 +1000

    icon_style now supports arbitrary styling rules :pushpin:

--- R/style.R
+++ R/style.R
@@ -2,17 +2,29 @@
 #'
 #' @param scale Scaled size of the icon.
 #' @param fill The colour for the fill.
+#' @param ... Other CSS rules for the icon style.
 #'
+#' @importFrom stringr str_replace
 #' @export
-style <- function(x, scale = NULL, fill = NULL){
+icon_style <- function(x, scale = NULL, fill = NULL, ...){
   style <- x$attribs$style
   if(!is.null(fill)){
-    style <- gsub("fill:[^;]*;", "", style)
-    style <- paste0(style, "fill:", fill, ";")
+    style <- str_replace(style, "fill:[^;]*;", "")
+    style <- glue("{style}fill:{fill};")
   }
   if(!is.null(scale)){
-    style <- gsub("height:[^;]*;", "", style)
-    style <- paste0(style, "height:", scale, "em;")
+    style <- str_replace(style, "height:[^;]*;", "")
+    style <- glue("{style}height:{scale}em;")
+  }
+  dots <- list(...)
+  if(!is_empty(dots)){
+    style <- Reduce(f = function(style, modify){
+      style <- str_replace(style, glue("{modify[[1]]}:[^;]*;"), "")
+      style <- glue("{style}{modify[[1]]}:{modify[[2]]};")
+      },
+      mapply(list, names(dots), dots, SIMPLIFY = FALSE),
+      init = style
+    )
   }
   x$attribs$style <- style
   x
