d142269aa7f3f759e45d140662799a3331bfe608
Mitchell <mail@mitchelloharawild.com>
2020-01-23 17:59:09 +1100

    :file_folder: Update default icon.path option

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
   op.icon <- list(
-    icon.path = rappdirs::user_data_dir("icon")
+    icon.path = rappdirs::user_data_dir("rpkg_icon")
   )
   toset <- !(names(op.icon) %in% names(op))
   if (any(toset)) options(op.icon[toset])
