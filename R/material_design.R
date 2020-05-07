d081972b4b4d57192f9c8ffa5222ea51dd429341
Mitchell <mail@mitchelloharawild.com>
2020-05-07 23:53:28 +1000

    :shirt: Finished adding material design icons

--- R/material_design.R
+++ R/material_design.R
@@ -12,11 +12,28 @@ download_material_design <- function(version = "dev"){
   meta <- jsonlite::read_json("https://raw.githubusercontent.com/google/material-design-icons/master/package.json")
 
   install_icon_zip(
-    "material_design", url, c("svg"),
+    "material_design", url, svg_path = mdi_svg_paths,
+    svg_pattern = "_48px\\.svg", svg_dest = mdi_svg_dest,
     meta = list(name = "Material Design Icons", version = meta$version, licence = meta$license)
   )
 
   invisible(material_design)
+}
+
+mdi_svg_paths <- function(path){
+  repo_dirs <- list.dirs(list.dirs(path, recursive = FALSE),recursive = FALSE)
+  svg_dirs <- Filter(function(x) "svg" %in% basename(list.dirs(x, recursive = FALSE)), repo_dirs)
+  file.path(svg_dirs, "svg")
+}
+
+mdi_svg_dest <- function(svgs){
+  svg_production <- basename(dirname(svgs)) == "production"
+  svg_group <- basename(dirname(dirname(dirname(svgs))))
+  svg_dest <- rep(NA_character_, length(svgs))
+  svg_dest[svg_production] <- file.path(
+    svg_group[svg_production],
+    gsub("^ic_", "", gsub("_48px\\.svg$", "\\.svg", basename(svgs[svg_production]))))
+  svg_dest
 }
 
 #' Material design icons
