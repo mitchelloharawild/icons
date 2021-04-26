4a336de70a2ac7e92c5a918ffb856a628d02e3d6
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2021-04-26 15:37:45 -0400

    :sparkles: Store and access Bioicons based on categories

--- R/bioicons.R
+++ R/bioicons.R
@@ -8,11 +8,14 @@ download_bioicons <- function() {
       simplifyVector = TRUE
     )
 
+  icon_df$category2 <- tolower(gsub("_", " ", icon_df$category))
+  icon_df$name2 <- tolower(icon_df$name)
+
   author <- purrr::map_chr(gsub(" ", "_", icon_df$author), utils::URLencode)
 
   name <- gsub(" ", "%20", icon_df$name)
 
-  cli::cat_bullet("Requesting Bioicons data from API", bullet = "info")
+  cli::cat_bullet("Requesting Bioicons data from web API", bullet = "info")
   bioicons <-
     purrr::pmap(
       list(
@@ -28,17 +31,38 @@ download_bioicons <- function() {
         )
     )
 
-  to_keep <- purrr::map_lgl(bioicons, ~ length(.x)>0 & is.raw(.x))
+  to_keep <- purrr::map_lgl(bioicons, ~ length(.x) > 0 & is.raw(.x))
 
   bioicons <- bioicons[to_keep]
+
+  categories <- unique(icon_df$category2)
 
   save_dir <-
     fs::path_real(fs::dir_create(rappdirs::user_data_dir('rpkg_icon/bioicons')))
 
-  save_paths <- glue::glue("{save_dir}/{icon_df$name}.svg")[to_keep]
+  fs::dir_create(
+    glue::glue("{rappdirs::user_data_dir('rpkg_icon/bioicons')}/{categories}")
+  )
 
-  cli::cat_bullet("Saving Bioicons as vector graphics", bullet = "info")
+  save_paths <- glue::glue("{save_dir}/{icon_df$category2}/{icon_df$name2}.svg")[to_keep]
+
+  cli::cat_bullet("Storing Bioicons as vector graphics", bullet = "info")
   purrr::walk2(bioicons, save_paths, rsvg::rsvg_svg)
+
+  cli::cat_bullet("Storing Bioicons metadata", bullet = "info")
+  saveRDS(
+    list(
+      name = "Bioicons",
+      version = "",
+      licence = c("BSD", "CC-0", "CC by SA", "MIT")
+    ),
+    glue::glue("{save_dir}/meta.rds")
+  )
+
+  saveRDS(
+    icon_df,
+    glue::glue("{rappdirs::user_data_dir('rpkg_icon/bioicons')}/icons.rds")
+  )
 
   invisible(bioicons)
 }
@@ -50,6 +74,14 @@ download_bioicons <- function() {
 #'   Illustrator.
 #'
 #' @param name Name of the icon.
+#' @param category The icon's category, one of "Animals", "Blood  immunology",
+#'   "Cell lines", "Cell membrane", "Chemistry", "Chemo- and Bioinformatics",
+#'   "General items", "Genetics", "Human physiology", "Intracellular
+#'   components", "Lab apparatus", "Machine Learning", "Microbiology",
+#'   "Nucleic Acids", "Oncology", "Parasites", "Receptor channels", "Safety
+#'   symbols", "Scientific graphs", "Tissues", or "Viruses". `category` is not
+#'   case-sensitive. If `NULL`, it will default to the first
+#'   available icon with this name.
 #'
 #' @seealso <https://bioicons.com/>
 #'
@@ -58,7 +90,21 @@ download_bioicons <- function() {
 #' @export
 bioicons <- new_icon_set(
   "bioicons",
-  function(name) {
-    icon_fn$get(name)
+  function(name, category = NULL) {
+    if (is.null(category)) {
+      info <-
+        readRDS(
+          glue::glue("{rappdirs::user_data_dir('rpkg_icon/bioicons')}/icons.rds")
+        )
+      icon_fn$get(
+        c(
+          info$category2[info$name2==tolower(name)][[1]],
+          info$name2[info$name2==tolower(name)][[1]]
+        )
+      )
+    }
+    else {
+      icon_fn$get(c(tolower(category), tolower(name)))
+    }
   }
 )
